ca53f4ecf9654850960646b88ff5d626e943fce3
Jonathan Salazar <jonanx779@hotmail.com>
2019-01-30 22:55:06 -0500

    The basics: let & var

---
+++ swift4.2/the_basics.playground/Contents.swift
@@ -0,0 +1,39 @@
+/* - - - - - - - - - - - - -
+ ; Jonathan Salazar Santos
+ ; The Basics
+ ; Reference: The Programming Language (Swift 4.1 beta)
+ ; 2019
+ ; - - - - - - - - - - - - - */
+
+/* Para comentarios, se usa esta forma que encapsula el texto que estas leyendo justo ahora.
+Normalmente esta forma de comentar, es para textos largos.
+
+ Antes de utilizar Xcode para programar una aplicación, primero usaremos para efectos de aprendizaje, el playground,
+ que nos sirve para programar y concentrarnos únicamente en el aprendizaje de las estructuras, sintaxis, etc.
+
+*/
+
+// Puedes utiilzar esta otra forma de comentarios, para comentarios cortos.
+
+import UIKit
+
+// Para definir integers, usamos la palabra reservada Int
+// Para números flotantes, usamos Double y Float
+// Para valores boleanos, usamos Bool
+// Y para cadenas o datos textuales, usamos String
+// También pudemos utilizar otros tipos de datos como Array, Set y Dictionary
+
+// Otro aspecto a considerar, es el buen uso de las constantes
+// Las tuplas son otro tipo de datos muy útil en Swift que permite pasar un conjunto de valores como una tupla y/o retornarla cómo respuesta.
+
+// Las constantes y las variables las debeoms declarar antes de utilizarlas.
+// Las constantes se declaran con la palabra let
+// Las variables se declaran con la palabra var
+
+// Esta es una constante
+let MyFirstVar = 10
+// Esta es una variable
+var str = "Hello, playground"
+
+// Es posible declarar en una sola línea más variables o constantes
+var t1 = 1, t2 = 2, t3 = 3
